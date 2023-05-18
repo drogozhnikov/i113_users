5f2d3569762c440059a8687047f8a439b9f89b0a
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-05-18 10:29:10 +0300

    Init Commit

---
+++ src/main/java/com/i113_users/I113UsersApplication.java
@@ -0,0 +1,13 @@
+package com.i113_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I113UsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I113UsersApplication.class, args);
+	}
+
+}
